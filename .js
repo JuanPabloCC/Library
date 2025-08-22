1f395dfa7eed24ab6529cdcc2ada36ab09613c90
Juan Pablo <juan.pablo.castano@outlook.com>
2025-08-22 09:48:14 -0500

    feat: add Books constructor function

---
+++ .js
@@ -0,0 +1,13 @@
+function Books(title, author, pages, read) {
+    if (!new.target){
+        throw Error("You must use the 'new' operator to call the constructor!")
+    }
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.status = this.read ? "read" : "not read yet";
+    this.info = function info() {
+        console.log(`${this.title} by ${this.author}, ${this.pages} pages, ${this.status}`);
+    }
+};
