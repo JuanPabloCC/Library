79a7e5787dbd93eba5f05fe5262193b861676161
Juan Pablo <juan.pablo.castano@outlook.com>
2025-08-23 16:59:48 -0500

    feat: add custom form validation

--- .js
+++ .js
@@ -15,12 +15,7 @@ function addToLibrary(title, author, pages, read){
     library.push(book);
 };
 
-let divinaComedia = new Book("La Divina Comedia", "Dante Alligheri", "500", false);
-let condeMontecristo = new Book("El Conde De Montecristo", "Alejandro Dumas", "1500", true);
-let viajeCentro = new Book("El Viaje Al Centro De La Tierra", "Julio Verne", "300", true);
-
 let library = new Array();
-library.push(divinaComedia, condeMontecristo, viajeCentro);
 
 function displayBooks(book) {
     const libraryDiv = document.querySelector(".library");
@@ -38,4 +33,40 @@ function displayBooks(book) {
 
 };
 
-displayBooks(divinaComedia);
+const dialog = document.querySelector("dialog");
+const addBookButton = document.querySelector(".addBook");
+const confirmButton = document.querySelector("#confirm-button");
+const closeButton = document.querySelector("#close-button");
+const bookForm = document.querySelector("#book-form");
+
+
+addBookButton.addEventListener("click", () => {
+    dialog.showModal();
+});
+
+confirmButton.addEventListener("click", (e) => {
+
+    const titleInput = bookForm.elements[0];
+    const authorInput = bookForm.elements[1];
+    const pagesInput = bookForm.elements[2];
+    const readInput = bookForm.elements[3];
+
+    const titleInputVal = titleInput.validity.valueMissing;
+    const authorInputVal = authorInput.validity.valueMissing;
+    const pagesInputVal = pagesInput.validity.valueMissing;
+    const readInputVal = readInput.validity.valueMissing;
+
+    if (!titleInputVal && !authorInputVal && !pagesInputVal && !readInputVal){
+        e.preventDefault();
+        addToLibrary(titleInput.value, authorInput.value, pagesInput.value, readInput.value);
+        displayBooks(library);
+        bookForm.reset();
+    } else {
+        console.log("algo no está bien")
+    }
+});
+
+closeButton.addEventListener("click", (e)=>{
+    e.preventDefault();
+    dialog.close();
+})
